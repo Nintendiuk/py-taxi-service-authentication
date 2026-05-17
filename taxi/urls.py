81dd4cbb716ed99335c7530e54840a31ea99c4ae
Nintendiuk <oleksii.rachynskyi@gmail.com>
2026-05-18 00:52:32 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from .views import (
@@ -21,6 +22,16 @@ urlpatterns = [
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+    path(
+        "accounts/login/",
+        LoginView.as_view(template_name="registration/login.html"),
+        name="login",
+    ),
+    path(
+        "accounts/logout/",
+        LogoutView.as_view(next_page="/"),
+        name="logout",
     ),
 ]
 
